9903428083207b3cf7c8cbe46bc3283094978e42
Usanso <save1086@naver.com>
2025-06-23 14:39:10 +0900

    Feat_무브 작성
    이슈 발생하여 수정 완료

--- Assets/LHE/Scripts/PlayerController.cs
+++ Assets/LHE/Scripts/PlayerController.cs
@@ -1,30 +1,86 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-using UnityEngine.WSA;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
-public class NewBehaviourScript : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class PlayerController : MonoBehaviour
 {
-    // 컨트롤 변수 (카운터 등)
+    [Header("이동")]
+    public float moveSpeed = 8f;
+    public float acceleration = 50f;
+    public float deceleration = 50f;
+    private float currentSpeed;
+    private bool facingRight = true;
+
+    [Header("그라운드 체크")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayerMask = 1<<6;
+
+    // 컨트롤 (카운터 등)
     private float jumpBufferCounter;
 
-    // 인풋 변수
+    // 인풋
     private Vector2 moveInput;
     private bool jumpInputDown;
     private bool jumpInput;
     private bool dashInput;
 
-    // 중복 방지 변수
+    // 중복 방지
     private float jumpBufferTime = 0.2f;
 
+    // 컴포넌트
+    private Rigidbody2D rb;
+    private Collider2D col;
+
+    // 점프용
+    private bool isGrounded;
+    private bool wasGrounded;
+
+    // 대쉬용
+    private bool isDashing;
+    private float dashCooldownLeft;
+
+    #region 유니티 주기
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        CheckGrounded();
+        // 벽체크
+        // 사다리
+        // 바닥(플랫폼, 블록 체크)
+
+        // 핸들러 (타이머 +, 인풋, )
+    }
+
+    void FixedUpdate()
+    {
+        // 핸들러 및 체크로 조건 확인 후에 실제 작동
+
+        Movement();
+
+    }
+
+    #endregion
 
     #region 인풋
-    public void OnMove(InputAction.CallbackContext context)
+
+    public void OnMove(InputValue context)
     {
-        moveInput = context.ReadValue<Vector2>();
+        moveInput = context.Get<Vector2>();
     }
+
+    //public void OnMove(InputAction.CallbackContext context)
+    //{
+    //    moveInput = context.ReadValue<Vector2>();
+    //}
+
 
     public void OnJump(InputAction.CallbackContext context)
     {
@@ -32,6 +88,7 @@ public class NewBehaviourScript : MonoBehaviour
         {
             // 누른 즉시
             // jumpInputDown = true;
+            // 중복 방지
             jumpBufferCounter = jumpBufferTime;
         }
         
@@ -46,8 +103,97 @@ public class NewBehaviourScript : MonoBehaviour
             dashInput = true;
         }
     }
-
     #endregion
+
+    #region 체크 및 타이머
+    /// <summary>
+    /// 땅에 있는지 체크
+    /// </summary>
+    void CheckGrounded()
+    {
+        // 착지 및 점프 타이밍 파악용
+        wasGrounded = isGrounded;
+        // 딛을 수 있는 땅은 레이어 그라운드
+        // 관통 못하는 땅을 구별하기 위해 테그로 사용
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayerMask);
+
+        if (isGrounded && !wasGrounded)
+        {
+            // 착지시 대쉬 쿨타임 초기화
+            dashCooldownLeft = 0f;
+        }
+    }
+
+    /// <summary>
+    /// 조작키 중복을 방지하기 위한 타이머들
+    /// </summary>
+    void HandleTimers()
+    {
+        if (isGrounded)
+        {
+            // coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            // coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (jumpBufferCounter > 0)
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if (dashCooldownLeft > 0)
+        {
+            dashCooldownLeft -= Time.deltaTime;
+        }
+
+        //if (isWallJumping)
+        //{
+        //    wallJumpingCounter -= Time.deltaTime;
+        //    if (wallJumpingCounter <= 0f)
+        //    {
+        //        isWallJumping = false;
+        //    }
+        //}
+    }
+    #endregion
+
+    #region Movement
+    void Movement()
+    {
+        float targetSpeed = moveInput.x * moveSpeed;
+
+        // 사다리 오르는 중일 경우 타겟 스피드 속도 조절하여 위 아래로만 적용
+
+        // 가속도 감속도 조절
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
+        currentSpeed = Mathf.MoveTowards(currentSpeed, targetSpeed, accelRate * Time.fixedDeltaTime);
+
+        rb.velocity = new Vector2(currentSpeed, rb.velocity.y);
+
+        // 방향키 방향에 따라 바라보기 
+        if (moveInput.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (moveInput.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    /// <summary>
+    /// 케릭터 보는 방향 뒤집기
+    /// </summary>
+    void Flip()
+    {
+        facingRight = !facingRight;
+        // 뒤집기
+        transform.Rotate(0, 180, 0);
+    }
+    #endregion
+
     // 점프 강도 조절 활용? // Interactions 사용
     //public void OnJump(InputAction.CallbackContext context)
     //{

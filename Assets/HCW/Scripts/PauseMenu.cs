2dd60d62821b7f31a39c6f398ba60002d291c448
CHEEWOO HONG <1974cldn@gmail.com>
2025-06-24 10:43:39 +0900

    [Feat] 일시정지 메뉴 구현, TMP 추가

--- Assets/HCW/Scripts/PauseMenu.cs
+++ Assets/HCW/Scripts/PauseMenu.cs
@@ -1,25 +1,61 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-namespace Util
+namespace Utill
 {
     public class PauseMenu : MonoBehaviour
     {
+        [SerializeField] private KeyCode pauseMenuKey = KeyCode.Escape;
         public GameObject pauseMenuPanel;
         public Slider soundSlider;
-
+        
         private bool isPaused = false;
 
         private void Start()
         {
             pauseMenuPanel.SetActive(false);
+
+            soundSlider.value = AudioListener.volume;
+            soundSlider.onValueChanged.AddListener(SetVolume);
         }
         void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Escape))
+            if (Input.GetKeyDown(pauseMenuKey))
             {
-                
+                if (isPaused)
+                {
+                    ResumeGame();
+                }
+                else
+                {
+                    PauseGame();
+                }
             }
+        }
+        public void PauseGame()
+        {
+            pauseMenuPanel.SetActive(true);
+            Time.timeScale = 0f; // 게임 시간을 0으로 만들어 일시정지
+            isPaused = true;
+        }
+
+        public void ResumeGame()
+        {
+            pauseMenuPanel.SetActive(false); // 메뉴 패널 비활성화
+            Time.timeScale = 1f; // 게임 시간을 원래대로
+            isPaused = false;
+        }
+
+        public void GoToLobby()
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("로비 씬 이름을 넣어야함");
+        }
+
+        public void SetVolume(float volume)
+        {
+            AudioListener.volume = volume;
         }
     }
 }

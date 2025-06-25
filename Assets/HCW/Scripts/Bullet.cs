4f8ead2800e7a286ea3c34f0a475585ead23d61d
CHEEWOO HONG <1974cldn@gmail.com>
2025-06-25 18:02:09 +0900

    [Fix] 골램 스크립트 수정, 애니메이션 추가
    골램 기본공격 발사위치를 애니메이션 트리거로 변경

---
+++ Assets/HCW/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HCW
+{
+    public class Bullet : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                // 데미지 처리?
+                Destroy(gameObject);
+            }
+            else if (other.CompareTag("Ground"))
+            {
+                Destroy(gameObject);
+            }
+
+        }
+
+        void Start()
+        {
+            Destroy(gameObject, 3f); // 기본적으로 일정 시간 후에 파괴
+        }
+    }
+}

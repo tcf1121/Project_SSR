b6754ec6a5738e6af305dcef921a1e7342753dd1
coffeelatt <ssongcoffee@gmail.com>
2025-06-27 20:13:19 +0900

    {feat} 기능 브릿지 작업

--- Assets/Utill/RandomPosCreater.cs
+++ Assets/Utill/RandomPosCreater.cs
@@ -76,12 +76,17 @@ namespace Utill
         {
             List<Vector2> randomPosList = new();
             float divideX = (max.x - min.x) / num;
+            
             for (int i = 0; i < num; i++)
                 randomPosList.Add(RandomGroundPos(
-                    new Vector2(min.x, min.y),
-                    new Vector2(min.x + (divideX * i), max.y)
+                    
+                    new Vector2(min.x + (divideX * i), min.y),
+                    new Vector2(min.x + (divideX * i + 1), max.y)
                     ));
-            return randomPosList;
+            {
+                Debug.Log(divideX);
+                return randomPosList;
+            }
         }
 
 
